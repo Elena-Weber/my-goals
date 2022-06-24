1b51de61fd30ad333c5830953ed55d593e1bc63d
Elena-Weber <crochetmiracles@gmail.com>
2022-06-24 18:38:25 -0400

    start todo list app

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{margin: 15, borderWidth: 1, borderColor: 'red', padding: 15}}>Hello!</Text>
-      <Text style={styles.h1}>Hello World!</Text>
-      <Button title="Click me!" />
+    <View style={styles.container} >
+      <Text style={{margin: 15, padding: 15}}>Hello, Goal-Setter!</Text>
+      <View style={styles.goalAdd} >
+        <TextInput placeholder='Type your goal here'  style={styles.input} />
+        <Button title="Add" />
+      </View>
+      
+      <Text style={styles.h1}>List of goals</Text>
+      
     </View>
   );
 }
@@ -14,13 +19,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  h1: {
+  goalAdd: {
     margin: 15,
+    padding: 15,
+    flexDirection: 'row'
+  },
+  input: {
+    // margin: 15,
+    padding: 5,
     borderWidth: 1,
-    borderColor: 'blue',
-    padding: 15
+    borderColor: '#cccccc',
+    width: '80%',
+    borderRadius: 8
   }
 });
